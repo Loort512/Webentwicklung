94e564ed92d844a64f5a89dbc1019faf0dfc49b2
loort813 <Artemis3785!>
2022-05-05 08:23:22 +0200

    backup

--- ManagementSystem/Frontend/src/main/router.js
+++ ManagementSystem/Frontend/src/main/router.js
@@ -4,35 +4,62 @@ function load_login() {
     document.getElementById("content").innerHTML='<object type="text/html" data="login.html" ></object>';
 }
 
+function load_fragment(target){
+    // if logged in as role the add folder in path
+    //load_fragment();
+    //$("#content").load("fragments/login.html")
+    document.getElementById("content").innerHTML='<object type="text/html" data="fragments/' + target + '.html" ></object>';
+} 
+
 // route to fragments
-function route(target=login) {
-    let isLogin = sessionStorage.getItem("loggedIn");
+function route(target="login") {
     console.log("route to: " + target);
-    
-    if(target === "login") {
-        // do login process
-        this.login();
-    }
-    if(isLogin !== "true") {
+    console.log("isLoggedIn: ", isLoggedIn())
+    if(!isLoggedIn()) {
         console.log("USER is not logged in -> route to login page");
         target = "login";
     }
     // if logged in as role the add folder in path
+    load_fragment(target)
     //load_fragment();
     //$("#content").load("./fragments/login.html")
-    document.getElementById("content").innerHTML='<object type="text/html" data="fragments/' + target + '.html" ></object>';
+    //document.getElementById("content").innerHTML='<object type="text/html" data="fragments/' + target + '.html" ></object>';
 }
 
-function load_fragment(){
-    var doc = document.querySelector('link[rel="import"]').import
-    var text = doc.querySelector('.doc');
+function login(type) {
+    console.log("login");
+    // filter data from frontend and reutrn if logged in
+    var mailStaff = "staff";
+    var pwdStaff = "staff";
+    var mailAdmin = "admin";
+    var pwdAdmin = "admin";
 
-    console.log("doc: ", doc);
-    console.log("text: ", text)
+    if(type === "staff") {
+        console.log("staff login")
+        var actualMailStaff = document.getElementById('usernameStaff').value;
+        var actualPwdStaff = document.getElementById('passwordStaff').value;
 
-    document.body.appendChild(text.cloneNode(true))
+        console.log("actual: ", actualMailStaff);
+        console.log("pwd:  ", actualPwdStaff);
+
+        if(actualMailStaff === mailStaff && actualPwdStaff === pwdStaff){
+            console.log("Staff login succeed")
+            setLoggedInToSession();
+            route("staffCourse");
+        } 
+    } 
+    
 } 
 
-function login() {
-    // filter data from frontend and reutrn if logged in
+function setLoggedInToSession(){
+    sessionStorage.setItem("loggedIn", "1");
+    console.log("changed sessionStorage: ", sessionStorage);
+} 
+function isLoggedIn(){
+    var sessionVar = sessionStorage.getItem("loggedIn");
+    console.log("sessionVar: ", sessionVar);
+    if(sessionVar === "1"){
+        return true;
+    } 
+    return false;
 } 
